5532a02e7981b2cc2efaca79a554934babbf8017
Gyb <helloholden@163.com>
2023-12-14 19:05:02 +0800

    [add]buff相关

--- Assets/Scripts/Buff/BuffManager.cs
+++ Assets/Scripts/Buff/BuffManager.cs
@@ -1,11 +1,22 @@
 ﻿using CommonBase;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace OfficeWar
 {
     public class BuffManager : MonoSingleton<BuffManager>
     {
+        public Dictionary<string, List<BuffInstance>> buffDic;
+        protected override void Awake()
+        {
+            base.Awake();
 
+        }
+
+        private void Update()
+        {
+
+        }
     }
 }
