92c17004e90e7561742c6139c5c3e0dbbadc4bd8
Gyb <479276132@qq.com>
2023-12-17 22:37:10 +0800

    [add]添加远程武器

---
+++ Assets/Scripts/Tasks/Bullet.cs
@@ -0,0 +1,31 @@
+﻿using BehaviorDesigner.Runtime.Tasks.DOTween;
+using UnityEngine;
+
+namespace OfficeWar
+{
+    public class Bullet : MonoBehaviour
+    {
+        public float damage = 15;
+        public float speed = 20;
+        public Health Owner { get; private set; }
+        private void Update()
+        {
+            this.transform.position += this.transform.right * Time.deltaTime * 20;
+        }
+
+        public void Init(Health owner)
+        {
+            this.Owner = owner;
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            var h = collision.GetComponent<Health>();
+            if (h && h != Owner)
+            {
+                h.BeHurt(damage, this.transform);
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Tasks/RangeWeapon.cs
@@ -0,0 +1,34 @@
+﻿using CommonBase;
+using UnityEngine;
+
+namespace OfficeWar
+{
+    public class RangeWeapon : BaseWeapon
+    {
+        public float attckRange;
+        //public GameObject bulletPref;
+        private Vector2 fireDir;
+
+
+        public override void Attack()
+        {
+            base.Attack();
+            //weaponAnimator.SetTrigger("Attack");
+            var mousePos = InputUtils.GetMouseWorldPosition();
+            var orginalPos = transform.localPosition;
+            var go = ObjectPoolManager.Instance.GetNextObject("子弹");
+            go.GetComponent<Bullet>().Init(Owner);
+            go.transform.SetPositionAndRotation(this.transform.position, this.transform.rotation);
+        }
+
+        protected override void Update()
+        {
+            if (IsAttacking)
+            {
+                return;
+            }
+            fireDir = (InputUtils.GetMouseWorldPosition() - this.transform.position).normalized;
+            transform.right = fireDir;
+        }
+    }
+}
