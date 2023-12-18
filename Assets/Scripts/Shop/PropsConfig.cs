dc1e93e80da8a90b859ad00bcc88f3633c465cdc
Gyb <helloholden@163.com>
2023-12-18 10:22:43 +0800

    [add]商店信息

---
+++ Assets/Scripts/Shop/PropsConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace OfficeWar
+{
+    public class PropsConfig : ScriptableObject
+    {
+        public string itemName;
+        public float criticalRate;
+        public float cd;
+        public float criticalProbability;
+    }
+}
