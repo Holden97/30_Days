ee827396767ae83181f2699b0dac984fca291803
Gyb <479276132@qq.com>
2023-12-24 12:22:47 +0800

    [add]准备界面

---
+++ Assets/Scripts/UI/PreparePanel.cs
@@ -0,0 +1,19 @@
+﻿using CommonBase;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OfficeWar
+{
+    /// <summary>
+    /// 人物初始准备面板
+    /// </summary>
+    public class PreparePanel : BaseUI
+    {
+
+        public override void UpdateView(object o)
+        {
+            base.UpdateView(o);
+        }
+    }
+}
