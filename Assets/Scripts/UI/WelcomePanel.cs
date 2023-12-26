b7384534c4b4150928f1dde7a33d88f858764c6f
Katrina0419 <1051942859@qq.com>
2023-12-26 12:57:31 +0800

    花里胡哨更新之：开发人员名单

--- Assets/Scripts/UI/WelcomePanel.cs
+++ Assets/Scripts/UI/WelcomePanel.cs
@@ -3,11 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 namespace OfficeWar
 {
     public class WelcomePanel : BaseUI
     {
+        [Header("背景")]
+        public GameObject BG;
+        [Header("Start")]
+        public GameObject start;
+        [Header("DevelopmentList")]
+        public GameObject developmentList;
+
         public void StartGame()
         {
             UIManager.Instance.ShowPanel<PreparePanel>(data: GameManager.Instance.weaponsData);
@@ -20,6 +28,32 @@ namespace OfficeWar
 #else
             Application.Quit();
 #endif
+        }
+
+
+        /// <summary>
+        /// 开发人员名单
+        /// </summary>
+        public void DevelopmentList()
+        {
+            start.SetActive(false);
+
+            BG.transform.DOScale(new Vector3(2, 2, 1), 1f);
+            BG.transform.DOLocalMove(new Vector3(-360, -540, 0), 1f);
+
+            Invoke("active", 1f);
+        }
+        void active()
+        {
+            developmentList.SetActive(true);
+        }
+        public void OpenIt()
+        {
+            start.SetActive(true);
+            developmentList.SetActive(false);
+
+            BG.transform.DOScale(new Vector3(1, 1, 1), 1f);
+            BG.transform.DOLocalMove(new Vector3(0, 0, 0), 1f);
         }
     }
 }
