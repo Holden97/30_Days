ffa582e2139eda89663a1d6efced9ff324891d9f
Gyb <479276132@qq.com>
2023-12-30 11:47:55 +0800

    [add]子弹回收

---
+++ Assets/Scripts/Tasks/ProjectileLifeTime.cs
@@ -0,0 +1,24 @@
+﻿using CommonBase;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace OfficeWar
+{
+    public class ProjectileLifeTime : MonoBehaviour
+    {
+        [Range(2, 5)] public float duration;
+
+        private void OnEnable()
+        {
+            StartCoroutine(lifeTimer());
+        }
+
+        private IEnumerator lifeTimer()
+        {
+            yield return new WaitForSeconds(duration);
+            this.gameObject.SetActive(false);
+            ObjectPoolManager.Instance.Putback("子弹", this.gameObject);
+        }
+    }
+}
